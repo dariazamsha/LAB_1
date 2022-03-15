4412953d0f2931e3804208f59c65cb1b17c029e5
Daria Zamsha <dariazamsha@gmail.com>
2022-03-15 15:21:19 +0100

    Metody "System.out"

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,6 +2,16 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java word\n");
+        System.out.print("Ala");
+        System.out.print("ma");
+        System.out.print("kota");
+
+        System.out.println("\nAla");
+        System.out.println("ma");
+        System.out.println("kota");
+
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
     }
 }
