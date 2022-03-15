f661b2b2a778f85a4b5089cde70e482ffe72c6f6
Daria Zamsha <dariazamsha@gmail.com>
2022-03-15 15:53:36 +0100

    Tabela funkcji sin dla kątów od 0 do 360 stopni co 10 stopni

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,6 +2,8 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-    System.out.println("\nNazywaliśmy to „witaminą B3”");
+    System.out.printf("alfa\t sin(alfa)\n");
+    for (int i = 0; i < 370; i+=10)
+        System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
     }
 }
