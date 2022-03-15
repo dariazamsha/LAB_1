d14f429ec6630db31820e56861a0919792b45e06
Daria Zamsha <dariazamsha@gmail.com>
2022-03-15 15:44:20 +0100

    Wyświetlenie tekstu na ekranie

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,10 +2,6 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        double b = 4.21;
-        String c = "Hello";
-        System.out.printf("a = %d, b = %f, c = %s %n",a,b,c);
-        System.out.printf("a = %d, b = %.2f, c = %20s %n",a,b,c);
+    System.out.println("\nNazywaliśmy to „witaminą B3”");
     }
 }
